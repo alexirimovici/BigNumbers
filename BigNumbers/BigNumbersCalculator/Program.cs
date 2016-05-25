9a3fd5cf516d131f5a08c1bcea9958c8e85750ac
alexirimovici - Alexandru Irimovici <alexandru.irimovici@gmail.com>
2016-05-25 13:50:55 +0300

    Added constructors, ToString and Console

--- BigNumbers/BigNumbersCalculator/Program.cs
+++ BigNumbers/BigNumbersCalculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BigNumbers;
 
 namespace BigNumbersCalculator
 {
@@ -10,6 +11,21 @@ namespace BigNumbersCalculator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                byte[] a = new byte[] { 0, 0, 5 };
+                byte[] b = new byte[] { 3, 3, 6 };
+                var aBig = new BigNumber(a);
+                Console.WriteLine(aBig);
+                var bBig = new BigNumber("00057");
+                Console.WriteLine(bBig);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
